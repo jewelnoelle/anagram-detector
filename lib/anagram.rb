ae53d1e1f75664593373a5c424ab3b1e168651c8
Jewel Carrier-Davis <jujicb@gmail.com>
2021-03-05 22:39:03 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    attr_accessor :word
+
+    def match(array)
+        array.select do |possible_anagram|
+            poss_an = possible_anagram.split(//)
+            poss_an.sort == word.split(//).sort
+        end
+    end
+end
